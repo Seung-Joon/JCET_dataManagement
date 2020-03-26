d6feaad0a76ac4f8a074c9d99e783bb9cc6142a9
Seung-Joon <tmdwns02556@gmail.com>
2020-03-26 12:18:59 +0900

    Edit exception enviroment

--- CASE1/Case1_Error_Report.py
+++ CASE1/Case1_Error_Report.py
@@ -18,40 +18,30 @@ connection = sql.Init.Connection(connectionString) #DB 연결
 class NoDataException(Exception):
     pass
 
-
 if __name__ == "__main__":
-    try:
-        #print("Case1 에러 데이터 받아오는중...\n")
-        dataSets = sql.SqlGet.Data_Case1Err_LotID(connection)
+    #print("Case1 에러 데이터 받아오는중...\n")
+    dataSets = sql.SqlGet.Data_Case1Err_LotID(connection)
+    if (len(list(dataSets)) == 0): raise NoDataException() #에러 처리된 데이터가 없을경우 에러 발생하여 레포트 생성 안되도록 함.
 
-        print("< Data Error Report. Generated Date: " + sys.argv[1] + ' ' + sys.argv[2] + " >\n")
-        print("Error Data List ↓\n")
+    print("< Data Error Report. Generated Date: " + sys.argv[1] + ' ' + sys.argv[2] + " >\n")
+    print("Error Data List ↓\n")
 
-        columns_id = [' [LOTID]', ' [Error Message]']
-        data = list(map(list, dataSets)) # COM_LOCATION 별 데이터를 리스트 형태로 저장함
-        print(pd.DataFrame(data, columns=columns_id)) # 데이터 차트 표시
-	
-        print('\n')
-        
-        for index in data:
+    columns_id = [' [LOTID]', ' [Error Message]']
+    data = list(map(list, dataSets)) # COM_LOCATION 별 데이터를 리스트 형태로 저장함
+    print(pd.DataFrame(data, columns=columns_id)) # 데이터 차트 표시
 
-            print("<" + index[0] + ">")
-            print("===============================================================================")
-            # COMPART가 NULL로 처리된 에러 데이터 가져옴
-            cursor = connection.cursor()
-            cursor.execute(sql.FormatSQL.Query_getDataComLocationNull(index[0]))
+    print('\n')
 
-            columns_id = ['     [EQUIP_ID]', '     [STRIP_ID]', '     [COM_LOCATION]', '     [CREATED_TIME]']
-            errData = list(map(list, cursor))
-            print(pd.DataFrame(errData, columns=columns_id)) # 데이터 차트 표시
-            print("\netc...")
-            print("===============================================================================\n")
+    for index in data:
+        print("<" + index[0] + ">")
+        print("===============================================================================")
+        # COMPART가 NULL로 처리된 에러 데이터 가져옴
+        cursor = connection.cursor()
+        cursor.execute(sql.FormatSQL.Query_getDataComLocationNull(index[0]))
 
+        columns_id = ['     [EQUIP_ID]', '     [STRIP_ID]', '     [COM_LOCATION]', '     [CREATED_TIME]']
+        errData = list(map(list, cursor))
+        print(pd.DataFrame(errData, columns=columns_id)) # 데이터 차트 표시
+        print("\netc...")
+        print("===============================================================================\n")
 
-        
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        print('End of Report.')
--- CASE1/Case1_Shell_Excuter.py
+++ CASE1/Case1_Shell_Excuter.py
@@ -10,26 +10,31 @@ generatorFile = workingAreaPath + "/Case1_Error_Report.py"
 outputFilePath =  workingAreaPath + '/output/ERROR_DATA_REPORT-'
 appSettingDataPath = workingAreaPath + '/AppSettings.json' 
 
-
-# 레포트 생성
-os.system(systemInterpreterPath + ' ' + generatorFile + ' ' + timeFlag.strftime("%Y-%m-%d, %H:%M:%S") + # Interpreter pythonFile argv1 argv2
-	   ' > ' + 
-	   outputFilePath  + timeFlag.strftime("%Y%m%d%H%M%S")) #레포트파일
-
-
-# 메일 정보 로딩
-with open(appSettingDataPath) as json_file:
-	appSettingData = json.load(json_file) # App Setting Data 불러옴
+if __name__ == '__main__':
+    try:
+        # 레포트 생성
+        os.system(systemInterpreterPath + ' ' + generatorFile + ' ' + timeFlag.strftime("%Y-%m-%d, %H:%M:%S") + # Interpreter pythonFile argv1 argv2
+                ' > ' +
+                outputFilePath  + timeFlag.strftime("%Y%m%d%H%M%S")) #레포트파일
 
 
-mail_Settings = appSettingData["mailSettings"]
-mail_Target = mail_Settings["addressee"] # 수신인 지정
-mail_carbonCopy = ",".join(mail_Settings["carbonCopy"]) # 참조 지정
+        # 메일 정보 로딩
+        with open(appSettingDataPath) as json_file:
+                appSettingData = json.load(json_file) # App Setting Data 불러옴
+
+        mail_Settings = appSettingData["mailSettings"]
+        mail_Target = mail_Settings["addressee"] # 수신인 지정
+        mail_carbonCopy = ",".join(mail_Settings["carbonCopy"]) # 참조 지정
 
 
-# 메일 전송
-os.system("mail -s \"Data Error Warning Case 1 - " +  timeFlag.strftime("%Y-%m-%d, %H:%M:%S") + "\" " + # 제목
-	   mail_Target + " " + # 수신인
-	   mail_carbonCopy + # 참조
-	   " < " + 
-	   outputFilePath + timeFlag.strftime("%Y%m%d%H%M%S")) #레포트 파일
+        # 메일 전송
+        os.system("mail -s \"Data Error Warning Case 1 - " +  timeFlag.strftime("%Y-%m-%d, %H:%M:%S") + "\" " + # 제목
+                mail_Target + " " + # 수신인
+                mail_carbonCopy + # 참조
+                " < " +
+                outputFilePath + timeFlag.strftime("%Y%m%d%H%M%S")) #레포트 파일
+
+
+    except:
+        pass
+
